853fa8f77de0665cfb162ff5d6f5a3a9e093abd6
Mateo Verbiest <ephec@vps-0b7ac9ac.vps.ovh.net>
2024-04-28 18:22:26 +0000

    Changing original images names to be used in docker swarm

--- woodytoys/services/build_push.sh
+++ woodytoys/services/build_push.sh
@@ -6,29 +6,29 @@ default_version="3"
 version=${1:-"$default_version"}
 
 
-docker build -t xdubruille/woody_api:"$version" api 
-docker tag xdubruille/woody_api:"$version" xdubruille/woody_api:latest
+docker build -t correba/woody_api:"$version" api 
+docker tag correba/woody_api:"$version" correba/woody_api:latest
 
-docker build -t xdubruille/woody_rp:"$version" reverse-proxy
-docker tag xdubruille/woody_rp:"$version" xdubruille/woody_rp:latest
+docker build -t correba/woody_rp:"$version" reverse-proxy
+docker tag correba/woody_rp:"$version" correba/woody_rp:latest
 
-docker build -t xdubruille/woody_database:"$version" database
-docker tag xdubruille/woody_database:"$version" xdubruille/woody_database:latest
+docker build -t correba/woody_database:"$version" database
+docker tag correba/woody_database:"$version" correba/woody_database:latest
 
-docker build -t xdubruille/woody_front:"$version" front
-docker tag xdubruille/woody_front:"$version" xdubruille/woody_front:latest
+docker build -t correba/woody_front:"$version" front
+docker tag correba/woody_front:"$version" correba/woody_front:latest
 
 
 # avec le "set -e" du début, je suis assuré que rien ne sera pushé si un seul build ne c'est pas bien passé
 
-docker push xdubruille/woody_api:"$version"
-docker push xdubruille/woody_api:latest
+docker push correba/woody_api:"$version"
+docker push correba/woody_api:latest
 
-docker push xdubruille/woody_rp:"$version"
-docker push xdubruille/woody_rp:latest
+docker push correba/woody_rp:"$version"
+docker push correba/woody_rp:latest
 
-docker push xdubruille/woody_front:"$version"
-docker push xdubruille/woody_front:latest
+docker push correba/woody_front:"$version"
+docker push correba/woody_front:latest
 
-docker push xdubruille/woody_database:"$version"
-docker push xdubruille/woody_database:latest
+docker push correba/woody_database:"$version"
+docker push correba/woody_database:latest
