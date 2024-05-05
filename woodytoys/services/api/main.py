c83aa587f939f702f2658f7519bc0b55a931c7d2
Correba <mat.correba@gmail.com>
2024-05-05 20:24:14 +0000

    Fixing redis connection error

--- woodytoys/services/api/main.py
+++ woodytoys/services/api/main.py
@@ -13,11 +13,24 @@ app = Flask('my_api')
 cors = CORS(app)
 
 redis_host = os.environ.get('REDIS_HOST')
+cache = None
 
-cache = redis.Redis(host=redis_host, port=6379)
-if not cache:
-    exit(1)
+def is_redis_available():
+    try:
+        cache.ping()
+    except (redis.exceptions.ConnectionError, redis.exceptions.BusyLoadingError):
+        return False
+    return True
 
+
+def redis_connect():
+    global cache
+    for x in range(3):
+        cache = redis.Redis(host=redis_host, port=6379)
+        if is_redis_available():
+            return True
+    return False
+    
 
 @app.get('/api/ping')
 def ping():
@@ -35,7 +48,8 @@ def get_heavy():
     # TODO TP9: cache ?
     name = request.args.get('name')
     key = f'heavy_{name}'
-    heavy = cache.get(key)
+    if redis_connect():
+        heavy = cache.get(key)
     if heavy is None:
         heavy = woody.make_some_heavy_computation(name)
         cache.setex(key, 3600, heavy)
@@ -62,15 +76,14 @@ def get_product(product_id):
 def get_last_product():
     # TODO TP9: put in cache ? cache duration ?
     key = 'last_product'
-    cached_last_product = cache.get(key)
+    if redis_connect():
+        cached_last_product = cache.get(key)
 
-    if cached_last_product is not None:
-        return f'cache: {datetime.now()} - {cached_last_product}'
-
-    else:
+    if not cached_last_product:
         last_product = woody.get_last_product()  # note: it's a very slow db query
         cache.setex(key, 3600, last_product)
         return f'db: {datetime.now()} - {last_product}'
+    return f'cache: {datetime.now()} - {cached_last_product}'
 
 
 # ### 3. Order Service
@@ -100,7 +113,8 @@ def process_order(order_id, order):
     # ...
     # ... do many check and stuff
     key_validation = f'validation_{order}'
-    status = cache.get(key_validation)
+    if redis_connect():
+        status = cache.get(key_validation)
     if not status:
         status = woody.make_heavy_validation(order)
         cache.setex(key_validation, 3600, status)
